0f502b35b2a187df6e7f79e368a0450ca1ba9f39
Moises benzan <>
2020-05-09 00:59:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from lib.brokers import XTBClient
+from lib.credentials.loaders import JsonPasswordCredentialLoader
+
+if __name__ == '__main__':
+    client = XTBClient()
+    credentials = JsonPasswordCredentialLoader().parse()
+    status = client.connect_with_credentials(credentials)
+
+    # target = client.get_available_symbols()[0]
+    # new_sym = client.get_symbol(target)
+    #
+    sym = client.get_symbol("USDJPY")
+    comm = client.get_commission(2.0, sym)
+    # print(target)
+    # print(new_sym)
+    print(comm)
